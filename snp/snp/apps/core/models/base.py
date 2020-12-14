c3385212a6c28cc8a17a54b1ca103ab22d346f6d
isik-kaplan <isik.kaplan@outlook.com>
2020-12-15 00:25:22 +0300

    core.Base.slug, django-lifecycle
    Add slug field that is autogenerated from id and that is universal to every object on core app, add django-lifecycle as a dependency.

--- snp/snp/apps/core/models/base.py
+++ snp/snp/apps/core/models/base.py
@@ -3,13 +3,25 @@ from uuid import UUID, uuid4
 
 from django.db import models
 from django.utils.translation import gettext_lazy as _
+from django_lifecycle import BEFORE_CREATE, LifecycleModel, hook
 from simple_history.models import HistoricalRecords
 
 
-class Base(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid4, editable=False, verbose_name=_("ID"))
+class Base(LifecycleModel):
+    id = models.UUIDField(primary_key=True, db_index=True, editable=False, default=uuid4, verbose_name=_("ID"))
+    slug = models.SlugField(db_index=True, null=True, blank=True, verbose_name=_("Slug"))
     created_at = models.DateTimeField(auto_now_add=True, db_index=True, verbose_name=_("Created At"))
     updated_at = models.DateTimeField(auto_now=True, db_index=True, verbose_name=_("Updated At"))
+
+    @hook(BEFORE_CREATE)
+    def create_slug_from_id(self):
+        # We want to use the slug in the urls, so we are adding them to the database for ease of access
+        # It might also make sense to create slug field and this hook in another mixin but I don't see
+        # the harm in creating a slug for every database object, we will be using this model even in m2m through tables
+        # which would allow us to serve the through models as activity in the users profile
+        # We would preferably switch this guy into a generated field in postgres as soon as django supports it
+        # instead of writing this inside a hook on application logic
+        self.slug = self._uuid2slug(self.id)
 
     history = HistoricalRecords(inherit=True)
 
